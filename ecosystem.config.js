25a44ba656d35af53f19ab7d231c741ec79a3e00
Yeremia Alfa Dio <raikkonendio@gmail.com>
2026-09-10 21:59:20 +0700

    feat: Update ecosystem config to load environment variables and improve error handling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,26 @@
+const path = require("path");
+const cwdPath = "/home/ubuntu/propresenter-bilingual-lyrics-api";
+
+// Load the environment variables
+const result = require("dotenv").config({ path: path.join(cwdPath, ".env") });
+
+// Debug check in terminal
+if (result.error) {
+  console.error("❌ PM2 Config Error: Could not load .env file!", result.error);
+} else {
+  console.log("✅ PM2 Config Success: Loaded .env file successfully.");
+}
+
 module.exports = {
   apps: [
     {
       name: "propresenter-bilingual-api",
       script: "dist/index.js",
-      cwd: __dirname,
+      cwd: cwdPath,
       instances: 1,
       exec_mode: "fork",
       env: {
+        ...process.env,
         NODE_ENV: "production",
       },
       autorestart: true,
